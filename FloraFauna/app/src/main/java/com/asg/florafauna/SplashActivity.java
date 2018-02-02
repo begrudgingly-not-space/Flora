e1308cdb6c265afadabc0500d3acec1725d4453e
Chris H <cph015@latech.edu>
2018-02-01 18:37:52 -0600

    testing

--- FloraFauna/app/src/main/java/com/asg/florafauna/SplashActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SplashActivity.java
@@ -9,8 +9,5 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        
-        //I <3 dr. cherry
     }
 }
