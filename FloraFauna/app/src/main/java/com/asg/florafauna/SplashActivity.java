3b28ae78b5f6f9ffd10f73bc676af352a3e485eb
Chris H <cph015@latech.edu>
2018-02-02 13:12:56 -0600

    Testing if works

--- FloraFauna/app/src/main/java/com/asg/florafauna/SplashActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SplashActivity.java
@@ -9,6 +9,5 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //testing if it works
     }
 }
