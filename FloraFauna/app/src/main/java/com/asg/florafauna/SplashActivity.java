e8b7b3f0d670d19e4556d1fd76d2d859c88030d6
MStevenTowns <MST039@latech.edu>
2018-02-01 18:16:36 -0600

    towns test 2

--- FloraFauna/app/src/main/java/com/asg/florafauna/SplashActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SplashActivity.java
@@ -9,6 +9,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+<<<<<<< .merge_file_V7nJij
         // Testing new
+=======
+
+        //Kiss my butt android studio!!!
+        //I <3 dr. cherry
+>>>>>>> .merge_file_Ir6Izm
     }
 }
