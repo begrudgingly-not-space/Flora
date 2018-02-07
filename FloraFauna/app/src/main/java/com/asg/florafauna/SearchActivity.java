8097ad34a7763ec77a72bdcbdbd3af52501a2bc9
Kaleigh Key <kaleigh.key@pioneerrx.com>
2018-02-06 18:12:41 -0600

    Added functionality of Splash Activity

--- FloraFauna/app/src/main/java/com/asg/florafauna/SearchActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SearchActivity.java
@@ -1,8 +1,31 @@
 package com.asg.florafauna;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by kkey on 2/1/2018.
  */
 
-public class SearchActivity {
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(SearchActivity.this, HelpActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void goToSettings(View view){
+        Intent intent = new Intent(SearchActivity.this, SettingsActivity.class);
+        startActivity(intent);
+
+    }
 }
