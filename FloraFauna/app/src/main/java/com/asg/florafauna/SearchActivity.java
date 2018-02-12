fefaa52b17fbf0491c27b9d767febd647495c82b
kaleighkey <kaleigh.key@pioneerrx.com>
2018-02-11 22:14:50 -0600

    Added custom action bar for Search Page; Included buttons for help and settings; Added small image (logo) to Splash Page

--- FloraFauna/app/src/main/java/com/asg/florafauna/SearchActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SearchActivity.java
@@ -2,7 +2,9 @@ package com.asg.florafauna;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 /**
@@ -15,17 +17,24 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.ab_search);
+
+        // Make action bar extend across entire width of screen
+        Toolbar toolbar = (Toolbar) getSupportActionBar().getCustomView().getParent();
+        toolbar.setContentInsetsAbsolute(0, 0);
+        toolbar.setPadding(0, 0, 0, 0);
     }
 
-    public void sendMessage(View view){
+    public void openHelp(View view){
         Intent intent = new Intent(SearchActivity.this, HelpActivity.class);
         startActivity(intent);
 
     }
 
-    public void goToSettings(View view){
+    public void openSettings(View view){
         Intent intent = new Intent(SearchActivity.this, SettingsActivity.class);
         startActivity(intent);
-
     }
 }
