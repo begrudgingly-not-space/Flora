ec7889368ec760427f876f8aa03ae26afb2635da
MStevenTowns <MST039@latech.edu>
2018-02-16 14:35:44 -0600

    moving code from main, creating functions now that main functionality works(a bit)

--- FloraFauna/app/src/main/java/com/asg/florafauna/WebTest.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/WebTest.java
@@ -18,14 +18,18 @@ public class WebTest
 
     private static Document doc;
     private static Response res;
-
     public static void main(String args[])
+    {
+        System.out.println(setEOLDescription("Ursus arctos"));
+    }
+    private static String setEOLDescription(String name)
     {
         try
         {
-            String query=eolQuery("Ursus arctos");
+            String query=eolQuery(name);
+            //String query=eolQuery("Ursus arctos");
             //URL page = new URL("http://eol.org/api/search/1.0.json?q=Ursus+arctos&page=1&exact=true&filter_by_taxon_concept_id=&filter_by_hierarchy_entry_id=&filter_by_string=&cache_ttl=");
-            System.out.println(query);
+            //System.out.println(query);
             URL page = new URL(query);
 
             URLConnection connection=page.openConnection();
@@ -33,23 +37,23 @@ public class WebTest
             connection.setDoInput(true);
             InputStream inStream = connection.getInputStream();
             BufferedReader input =new BufferedReader(new InputStreamReader(inStream));
-            String line;
-            while ((line = input.readLine()) != null)
-            {
-                //System.out.println(line);//this is a json
-                int start = line.indexOf("http");
-                int stop = line.indexOf("?");
-                String link= line.substring(start,stop);
-                res = Jsoup.connect(link).timeout(10000).execute();
-                doc=res.parse();
-                String hold=doc.toString();
-                start=hold.indexOf("<h4>Description</h4>")+21;
-                stop=hold.indexOf("\n", start);
-                String description=hold.substring(start,stop);
-            }
+            String line=input.readLine();
+
+            int start = line.indexOf("http");
+            int stop = line.indexOf("?");
+            String link= line.substring(start,stop);
+            res = Jsoup.connect(link).timeout(10000).execute();
+            doc=res.parse();
+            String hold=doc.toString();
+            start=hold.indexOf("<h4>Description</h4>")+21;
+            stop=hold.indexOf("\n", start);
+            String description=hold.substring(start,stop);
+            return description;
         }
-        catch(Exception e){
+        catch(Exception e)
+        {
             System.out.println(e.toString());
+            return "error";
         }
     }
     private static String eolQuery(String name)
