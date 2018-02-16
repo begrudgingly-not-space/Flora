80003869ebd92e89d33496e06a57382ec37895ab
MStevenTowns <MST039@latech.edu>
2018-02-16 17:08:45 -0600

    Changed to object

--- FloraFauna/app/src/main/java/com/asg/florafauna/SpeciesInfo.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SpeciesInfo.java
@@ -5,47 +5,45 @@ package com.asg.florafauna;
  */
 
 import org.jsoup.*;
-import org.jsoup.nodes.*;
-import org.jsoup.Connection.Response;
 
-public class WebTest
+public class SpeciesInfo
 {
+    String name;
+    String eolLink;
+    String description;
 
-    private static Document doc;
-    private static Response res;
-    public static void main(String args[])
+    public SpeciesInfo(String name)
     {
-        System.out.println(setEOLDescription("Panthera leo"));
+        this.name=name;
     }
-    private static String setEOLDescription(String name)
+    private void setEOLDescription(String name)
     {
         try
         {
             String query=eolQuery(name);
 
             String json = Jsoup.connect(query).ignoreContentType(true).execute().body();
-            String link = json.substring(json.indexOf("http"),json.indexOf("?"));
+            eolLink = json.substring(json.indexOf("http"),json.indexOf("?"));
 
-            String page = Jsoup.connect(link).timeout(10000).execute().parse().toString();
+            String page = Jsoup.connect(eolLink).timeout(10000).execute().parse().toString();
             int start = page.indexOf("</h4>",page.indexOf("<h4>Description"))+6;
             int stop = page.indexOf("\n", start);
-            String description=page.substring(start,stop);
-            return description;
+            description=page.substring(start,stop);
+            //return description;
         }
         catch(Exception e)
         {
             System.out.println(e.toString());
-            return "error";
         }
     }
-    private static String eolQuery(String name)
+    private String eolQuery(String name)
     {
         String first="http://eol.org/api/search/1.0.json?q=";
         String last="&page=1&exact=true&filter_by_taxon_concept_id=&filter_by_hierarchy_entry_id=&filter_by_string=&cache_ttl=";
         name=name.replaceAll(" ","+");
         return first+name+last;
     }
-    private static String gbifQuery(String name)
+    private String gbifQuery(String name)
     {
         String first = "http://api.gbif.org/v1/species?name=";
         name=name.replaceAll(" ","+");
