762a712fa95a7666905424f422b60ec9262c6890
kaleighkey <kaleigh.key@pioneerrx.com>
2018-02-16 20:26:25 -0600

    Deleted SearchByCounty file

--- FloraFauna/app/src/main/java/com/asg/florafauna/SearchActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SearchActivity.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
