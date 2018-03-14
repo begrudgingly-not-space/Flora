266856aa6a342467a474794c15108d4f4ce1bbca
kaleighkey <kaleigh.key@pioneerrx.com>
2018-03-14 11:58:45 -0500

    Fixed merging of aspects of SpeciesInfoActivity (species name being passed through)

--- FloraFauna/app/src/main/java/com/asg/florafauna/SpeciesInfoActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SpeciesInfoActivity.java
@@ -6,43 +6,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import static com.asg.florafauna.SearchActivity.INTENT_EXTRA_SPECIES_NAME;
+
 /**
  * Created by steven on 3/2/18.
  */
 
 public class SpeciesInfoActivity extends AppCompatActivity
 {
-    //copied from HelpActivity.java, no clue how this works
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speciesinfo);
-
         //default variables to take values from the results menu from the search/history
         String sName;
         String link;
 
-        //pull values passed by previous page
-        //stolen from:
-        //https://stackoverflow.com/questions/2091465/how-do-i-pass-data-between-activities-in-android-application#7325248
-        Bundle extras = getIntent().getExtras();
-        if (extras != null)
-        {
-            //link=extras.getString("link");
-            sName=extras.getString("sName");
-        }
-        else //default values for testing before pages are linked properly
-        {
-            sName="Ursus arctos";
-            //link="http://eol.org/pages/326447/overview";
-        }
+        String speciesName = getIntent().getStringExtra(INTENT_EXTRA_SPECIES_NAME);
 
+        if (speciesName != null) {
+            sName = speciesName;
+            // link = extras.getString("link");
+        }
+        else {
+            sName = "No species name";
+            // link = "http://eol.org/pages/326447/overview";
+        }
+        
         //create object for the animal selected
         SpeciesInfo test = new SpeciesInfo(sName);
 
-        //copied from stackoverflow to set the text in the ScientificName section to what i want
-        //stolen from:
-        //https://stackoverflow.com/questions/5821051/how-to-display-the-value-of-a-variable-on-the-screen#5821117
         TextView SNgetText = findViewById(R.id.ScientificName);
         SNgetText.setText(String.valueOf(test.getScientificName()));
 
@@ -59,7 +53,7 @@ public class SpeciesInfoActivity extends AppCompatActivity
         ELgetText.setText(String.valueOf(test.getEolLink()));
 
         //action bar creation copied form HelpActivity.java
-        FloraFaunaActionBar.createActionBar(getSupportActionBar(), R.layout.ab_help);
+        FloraFaunaActionBar.createActionBar(getSupportActionBar(), R.layout.ab_speciesinfo);
 
     }
     //opens settings
@@ -74,6 +68,7 @@ public class SpeciesInfoActivity extends AppCompatActivity
     }
 
     public void goBack(View view){
+        /* closes the activity */
         finish();
     }
 }
