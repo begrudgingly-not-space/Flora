3c21ff3eef4f60aba856fd86646f16763c9f81d1
b-swift <brad.alan.ewing@gmail.com>
2018-03-23 21:00:17 -0500

    Can now view sightings of species in states and counties.

--- FloraFauna/app/src/main/java/com/asg/florafauna/MapActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/MapActivity.java
@@ -17,7 +17,9 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ScrollView;
+import android.widget.Spinner;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -32,6 +34,9 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import static com.asg.florafauna.CountyFinder.countyFinder;
+import static com.asg.florafauna.StateFinder.stateFinder;
+
 
 /**
  * Created by brada on 3/13/2018.
@@ -41,8 +46,8 @@ public class MapActivity extends AppCompatActivity{
 
     WebView myWebView;
     String points = "-91.69000244140625 31.219999313 -90.00507354736328 30.337696075439453 -93.58332824707031 32.58332824707031 -89.84539794921875 30.270082473754883";
-
-
+    private EditText speciesInput;
+    private EditText locationInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +58,8 @@ public class MapActivity extends AppCompatActivity{
         FloraFaunaActionBar.createActionBar(getSupportActionBar(), R.layout.ab_map);
 
         myWebView = (WebView) findViewById(R.id.webview);
-
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-
 
         myWebView.loadUrl("file:///android_asset/map.html");
         myWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
@@ -76,6 +79,9 @@ public class MapActivity extends AppCompatActivity{
                 return myWebView.onTouchEvent(event);
             }
         });
+
+        speciesInput = findViewById(R.id.SearchEditText);
+        locationInput = findViewById(R.id.SearchEditTextRegion);
 
     }
 
@@ -120,14 +126,133 @@ public class MapActivity extends AppCompatActivity{
     }
 
     String bisonpoints = "";
-    public void searchBison(View view){
+    public void sightingsSearch(View view){
         bisonpoints = "";
-        getPointsFromBison(this);
+        //getPointsFromBison(this);
+
+        String species = speciesInput.getText().toString();
+        String location = locationInput.getText().toString();
+
+        Spinner spinner = findViewById(R.id.search_selection);
+        String selection = spinner.getSelectedItem().toString();
+        Log.d("spinner", selection);
+
+        String searchType = "";
+
+        if (selection.equals("Common name by state")){
+            Log.d("selection", "common state search");
+            searchType = "common_name";
+            sightingsByState(this, searchType, species, location);
+        }
+        else if (selection.equals("Common name by county")){
+            Log.d("selection", "common county search");
+            searchType = "common_name";
+            sightingsByCounty(this, searchType, species, location);
+        }
+        else if (selection.equals("Scientific name by state")){
+            Log.d("selection","scientific state search");
+            searchType = "scientific_name";
+            sightingsByState(this, searchType, species, location);
+        }
+        else {
+            Log.d("selection", "scientific county search");
+            searchType = "scientific_name";
+            sightingsByCounty(this, searchType, species, location);
+        }
+
     }
 
-    public void getPointsFromBison(Context context){
-        String url = "https://bison.usgs.gov/api/search.json?species=Mimus%20polyglottos&type=scientific_name&state=Louisiana&countyFips=22015&start=0&count=500";
+    public void sightingsByState(Context context, String searchType, String species, String location){
+        String state = "";
 
+        if (location.length() > 2) {
+            state = location.substring(0, 1).toUpperCase() + location.substring(1);
+            state = state.replaceAll(" ", "%20");
+        }
+        else if (location.length() == 2) {
+            state = location.substring(0,2).toUpperCase();
+            state = stateFinder(context, state);
+        }
+
+        species = species.replaceAll(" ", "%20");
+
+        String url = "https://bison.usgs.gov/api/search.json?species=" + species + "&type=" + searchType + "&state=" + state + "&start=0&count=500";
+        Log.d("url", url);
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        JsonObjectRequest pointsFromBison = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+
+                        try {
+
+                            final JSONArray speciesArray = response.getJSONArray("data");
+
+                            for(int i = 0; i < speciesArray.length(); i++) {
+                                String latitude = speciesArray.getJSONObject(i).getString("decimalLatitude");
+                                String longitude = speciesArray.getJSONObject(i).getString("decimalLongitude");
+                                bisonpoints = bisonpoints + longitude + " " + latitude + " ";
+                                Log.d("latitude", bisonpoints.length() + "a");
+                            }
+
+                            Log.d("latitude", bisonpoints.length() + "a");
+                            bisonpoints = bisonpoints.substring(0, bisonpoints.length() - 1);
+                            myWebView.reload();
+
+                        }
+                        catch (JSONException error) {
+                            Log.e("searchResponseException", error.toString());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("onErrorResponse", error.toString());
+
+            }
+        }
+        );
+
+        // Adds request to queue which is then sent
+        requestQueue.add(pointsFromBison);
+
+        //bisonpoints = bisonpoints.substring(0, bisonpoints.length() - 1);
+        //Log.d("bisonpoints", bisonpoints.length() + "a");
+
+        //myWebView.reload();
+    }
+
+    public void sightingsByCounty(Context context, String searchType, String species, String location){
+        String searchTerms[];
+        String county = "";
+        String state = "";
+
+        if (location.contains(",") && location.length() >= 3) {
+            searchTerms = location.split(",");
+
+            if (searchTerms.length == 2) {
+                county = searchTerms[0];
+                state = searchTerms[1];
+            }
+        }
+
+        if (state.length() > 3) {
+            state = state.substring(1, 2).toUpperCase() + state.substring(2);
+        }
+        else if (state.length() == 3) {
+            state = state.substring(1,3).toUpperCase();
+            state = stateFinder(context, state);
+        }
+
+        String countyFips = countyFinder(context, state, county);
+
+        species = species.replaceAll(" ", "%20");
+
+        state = state.replaceAll(" ", "%20");
+
+        String url = "https://bison.usgs.gov/api/search.json?species=" + species + "&type=" + searchType + "&state=" + state + "&countyFips=" + countyFips + "&start=0&count=500";
 
         RequestQueue requestQueue = Volley.newRequestQueue(context);
 
