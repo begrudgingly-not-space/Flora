4944400347ebef92a6b0637fdc373d08c7058654
nathan <nate.cooley101@gmail.com>
2018-03-25 18:54:23 -0500

    Merged master onto ncooley.
    Search Activity:
     - included MenuInflater
     - Overrided onCreateOptionsMenu
     - Overrided onOptionsItemSelect
     - Moved navigational buttons to overflow menu instead of action bar
    
    Settings Activity:
     - included MenuInflater
     - Overrided onCreateOptionsMenu
     - Overrided onOptionsItemSelect
     - Moved navigational buttons to overflow menu instead of action bar
    
    Help Activity:
     - Included MenuInflater
     - Overrided onCreateOptionsMenu
     - Overrided onOptionsItemSelect
     - Moved navigational buttons to overflow menu instead of action bar
    
    SpeciesInfo Activity:
     - Included MenuInfater
     - Overrided onCreateOptionsMenu
     - Overrided onOptionsItemSelect
     - Moved navigational buttons to overflow menu instead of action bar
     - Created res/menus/species_menu.xml

--- FloraFauna/app/src/main/java/com/asg/florafauna/SettingsActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SettingsActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -16,6 +17,29 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         FloraFaunaActionBar.createActionBar(getSupportActionBar(), R.layout.ab_settings);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                Intent search_intent = new Intent(SettingsActivity.this, SearchActivity.class);
+                startActivity(search_intent);
+                return true;
+            case R.id.action_help:
+                Intent help_intent = new Intent(SettingsActivity.this, HelpActivity.class);
+                startActivity(help_intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     public void openHelp(View view){
