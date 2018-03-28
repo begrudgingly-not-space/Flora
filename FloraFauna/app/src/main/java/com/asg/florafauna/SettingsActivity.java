e555ebea2ac5b288c5c6a7b8dd3fb539219dd374
Chris H <cph015@latech.edu>
2018-03-28 16:13:44 -0500

    Added functionality to save settings to files

--- FloraFauna/app/src/main/java/com/asg/florafauna/SettingsActivity.java
+++ FloraFauna/app/src/main/java/com/asg/florafauna/SettingsActivity.java
@@ -9,9 +9,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -21,7 +28,70 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         FloraFaunaActionBar.createActionBar(getSupportActionBar(), R.layout.ab_settings);
+
+
+        // save mileage
+        Spinner mileage = (Spinner) findViewById(R.id.miles);
+        mileage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String text = parent.getItemAtPosition(pos).toString();
+
+                try{
+                    FileOutputStream fOut = openFileOutput("mileage", MODE_PRIVATE);
+                    OutputStreamWriter osw = new OutputStreamWriter(fOut);
+                    osw.write(text);
+                    osw.flush();
+                    osw.close();
+                    fOut.close();
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    Log.e("Exception", "Failed to save history: " + e.toString());
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        // save style/theme
+        Spinner theme = (Spinner) findViewById(R.id.Style);
+        theme.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                String text = parent.getItemAtPosition(pos).toString();
+
+                try{
+                    FileOutputStream fOut = openFileOutput("theme", MODE_PRIVATE);
+                    OutputStreamWriter osw = new OutputStreamWriter(fOut);
+                    osw.write(text);
+                    osw.flush();
+                    osw.close();
+                    fOut.close();
+                }
+                catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    Log.e("Exception", "Failed to save history: " + e.toString());
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -79,6 +149,29 @@ public class SettingsActivity extends AppCompatActivity {
         file.delete();
         Log.d("Clear History", "History cleared.");
         Toast.makeText(this,"Clear History", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void saveSettings(){
+
+
+
+
+
+/*
+        try{
+            FileOutputStream fOut = openFileOutput("history", MODE_PRIVATE);
+            fOut.write(database);
+            fOut.flush();
+            fOut.close();
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            Log.e("Exception", "Failed to save history: " + e.toString());
+        }
+*/
     }
 
 }
